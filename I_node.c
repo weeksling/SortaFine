eb4d47922a6c5b29c39f0cfb87801a0db9e31a93
Matthew Weeks <weeksling@gmail.com>
2013-11-07 17:29:27 -0500

    Changed Stuff

--- I_node.c
+++ I_node.c
@@ -2,43 +2,49 @@
 //Sortafine Industries
 #include blockio.h
 
-compare_component_tobuff
-	
-get_empty_blk
+#define BLOCKS 8
+#define COMPONENTS 64
+
+// 
+int* i_node_table[8][64]; 
+
+get_empty_blk(void)
 	// dependant on put_super_blk
 	// dependant on get_super_blk
-get_fd
+get_fd(void)
 	// dependant on get_block
-get_file
+get_file(void)
 	// dependant on get_block
-get_file_pointer
+get_file_pointer(void)
 	// get_block
-get_inode_table
+get_inode_table(void)
 	// get_block
-get_refrence_count
+get_refrence_count(void)
 	// get_block
-get_super_blk
+get_super_blk(void)
 	// get_block
-put_file
+put_file(void)
 	// get_empty_blk
 	// put_inode_table
 	// put_block
-put_inode_table
+put_inode_table(void)
 	// put_block
-put_super_blk
+put_super_blk(void)
 	// put_block
-release_allblocks_fromfile
+release_allblocks_fromfile(void)
 	// release_block
 	// put_inode_table
-release_block
+release_block(void)
 	// get_super_blk
 	// put_super_blk
-set_fd
+set_fd(void)
 	// get_block
 	// put_block
-set_file_pointer
+set_file_pointer(void)
 	// get_block
 	// put_block
-set_refrence_count
+set_refrence_count(void)
 	// get_block
 	// put_block
+
+compare_component_tobuff(void);
