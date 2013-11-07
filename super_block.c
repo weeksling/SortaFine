68a7701ea8d9efdfb86e33707a78f54488bdf736
Matthew Weeks <weeksling@gmail.com>
2013-11-07 14:15:34 -0500

    Intitial upload of super_block.c

---
+++ super_block.c
@@ -0,0 +1,21 @@
+//super_block.c
+//Sortafine Industries
+
+int get_super_blk(void);
+
+int put_super_blk(void);
+
+
+int get_super_blk(void){
+	//Encode each 4 locations of diskbitmap[512] into an integer and puts
+	// it in super_blk_buf[128]
+	// Puts super_blk_buf[128] into the super-block
+}
+
+int put_super_blk(void){
+	//Get super-block (blocks 0 and 1 of disk) as "short integer bytes" and
+	// puts them in the super_blk_buf[128]
+
+	// Decodes each integer (<15 & >0) in super_blk_buf[128] into 4
+	// bits and puts them in disk_bitmap[512]
+}
