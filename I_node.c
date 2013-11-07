7c86cb41e65b7ac9acf9cfb9f4515d3d67617bab
Matthew Weeks <weeksling@gmail.com>
2013-11-07 14:43:09 -0500

    Commenting on I_node.c

--- I_node.c
+++ I_node.c
@@ -4,40 +4,40 @@
 compare_component_tobuff
 	
 get_empty_blk
-	put_super_blk
-	get_super_blk
+	// dependant on put_super_blk
+	// dependant on get_super_blk
 get_fd
-	get_block
+	// dependant on get_block
 get_file
-	get_block
+	// dependant on get_block
 get_file_pointer
-	get_block
+	// get_block
 get_inode_table
-	get_block
+	// get_block
 get_refrence_count
-	get_block
+	// get_block
 get_super_blk
-	get_block
+	// get_block
 put_file
-	get_empty_blk
-	put_inode_table
-	put_block
+	// get_empty_blk
+	// put_inode_table
+	// put_block
 put_inode_table
-	put_block
+	// put_block
 put_super_blk
-	put_block
+	// put_block
 release_allblocks_fromfile
-	release_block
-	put_inode_table
+	// release_block
+	// put_inode_table
 release_block
-	get_super_blk
-	put_super_blk
+	// get_super_blk
+	// put_super_blk
 set_fd
-	get_block
-	put_block
+	// get_block
+	// put_block
 set_file_pointer
-	get_block
-	put_block
+	// get_block
+	// put_block
 set_refrence_count
-	get_block
-	put_block
+	// get_block
+	// put_block
