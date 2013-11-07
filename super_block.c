33ab3ada645c15d9176ccab0d04116b01cc94d84
Adam <adam.a.pacheco@gmail.com>
2013-11-07 16:00:45 -0500

    Turned super_block to be about super blocks

--- super_block.c
+++ super_block.c
@@ -1,18 +1,18 @@
 //super_block.c
 //Sortafine Industries
 
-int get_blk(void);
+int get_super_blk(void);
 
-int put_blk(void);
+int put_super_blk(void);
 
 
-int get_blk(void){
+int get__super_blk(void){
 	//Encode each 4 locations of diskbitmap[512] into an integer and puts
 	// it in super_blk_buf[128]
 	// Puts super_blk_buf[128] into the super-block
 }
 
-int put_blk(void){
+int put_super_blk(void){
 	//Get super-block (blocks 0 and 1 of disk) as "short integer bytes" and
 	// puts them in the super_blk_buf[128]
 
