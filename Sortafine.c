b088e964157cd75f24258c534eed1d361bc60b5e
BlairWiser <blair.m.wiser@gmail.com>
2013-11-07 22:16:25 -0500

    added function inputs in their declarations

--- Sortafine.c
+++ Sortafine.c
@@ -2,19 +2,19 @@
 //Sortafine Industries
 #include blockio.h
 
-sfs_read
+void sfs_read(int fd, int start, int length, char* mem_pointer)
 	get_inode_table
 	get_fd
 	get_file
 	get_file_pointer
-sfs_write
+void sfs_write(int fd, int start, int length, char* mem_pointer)
 	get_file_pointer
 	get_inode_table
 	get_fd
 	get_file
 	set_file_pointer
 	put_file
-sfs_open
+void sfs_open(char* pathname)
 	get_reference_count
 	get_inode_table
 	compare_component_tobuff
@@ -22,12 +22,12 @@ sfs_open
 	get_file
 	set_reference_count
 	set_fd
-sfs_close
+void sfs_close(int fd)
 	get_fd
 	get_reference_count
 	set_fd
 	set_reference_count
-sfs_create
+void sfs_create(char* pathname)
 	get_inode_table
 	compare_component_tobuff
 	get_file_pointer
@@ -35,7 +35,7 @@ sfs_create
 	set_file_pointer
 	get_empty_blk
 	put_inode_table
-sfs_delete
+void sfs_delete(char* pathname)
 	get_inode_table
 	get_file_pointer
 	compare_component_tobuff
@@ -44,7 +44,7 @@ sfs_delete
 	set_file_pointer
 	release_allblocks_fromfiles
 	put_file
-sfs_initilize
+void sfs_initilize(int erase)
 	get_inode_table
 	get_super_blk
 	put_block
