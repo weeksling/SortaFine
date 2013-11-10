2299ab2757274db954f4a506552b8a224733e645
Matthew Weeks <weeksling@gmail.com>
2013-11-10 12:46:08 -0500

    I worked on shtuff for inode!

--- I_node.c
+++ I_node.c
@@ -1,64 +1,73 @@
 //I_node.c
 //Sortafine Industries
-#include <blockio.h>
+#include "blockio.h"
 #include <string.h>
+#include <stdlib.h>
+#include <malloc.h>
 
 // Common Values //
 #define BLOCKS 8
 #define COMPONENTS 64
 #define I_NODE_START 2
 #define FILE_POINTER 10
-
-
-// Global Variables //
-int i_node_table[BLOCKS][COMPONENTS]; 
-char* buff=(char*)calloc(1024*sizeof(char));
-int* file_pointer=(char*)calloc(COMPONENTS*sizeof(int));
+#define BUFFER_SIZE 1024
 
 // Function Declarations //
-get_fd(void);
+void get_fd(void);
 	// get_block
-get_file(void);
+void get_file(void);
 	// get_block
-get_file_pointer(int i_number, int* file_ptr);
+int get_file_pointer(int i_number, int* file_ptr);
 	// get_block
-get_inode_table(void);
+void get_inode_table(void);
 	// get_block
-get_refrence_count(void);
+void get_refrence_count(void);
 	// get_block
-get_super_blk(void);
+void get_super_blk(void);
 	// get_block
-put_file(void);
+void put_file(void);
 	// get_empty_blk
 	// put_inode_table
 	// put_block
-put_inode_table(void);
+int put_inode_table(void);
 	// put_block
-set_fd(void);
+void set_fd(void);
 	// get_block
 	// put_block
-set_file_pointer(void);
+void set_file_pointer(void);
 	// get_block
 	// put_block
-set_refrence_count(void);
+void set_refrence_count(void);
 	// get_block
 	// put_block
 int compare_component_tobuff(char *comp, char *buff);
 
+// Global Variables //
+int i_node_table[BLOCKS][COMPONENTS]; 
+char* buff=NULL;
+int* file_pointer=NULL;
+
 // Function Definitions //
+int main (){
+	buff=(char*)calloc(BUFFER_SIZE,sizeof(char));
+	file_pointer=(int*)calloc(64,COMPONENTS*sizeof(int));
+}
 int compare_component_tobuff(char *comp, char *buff){
-	return strncmp(comp,buff);
+	return strcmp(comp,buff);
 }
 
 int put_inode_table(){
-	for (int file=0; i<BLOCKS; i++){
-		put_block(I_NODE_START+i, i_node_table[i]);
+	for (int file=0; file<BLOCKS; file++){
+		buff=(char*)i_node_table;
+		put_block(I_NODE_START+file, buff);
 	}
 }
 
 int get_file_pointer(int i_number, int* file_ptr){
 	// file_pointer is the 10th block
-	get_block(FILE_POINTER, buff);
-	file_ptr=(int)file_pointer[i_number];
-	return file_ptr;
+	get_block(FILE_POINTER, (char*)file_ptr);
+	/*int* temp = (int*) buff;
+	file_ptr = temp[i_number];
+	free(temp);*/
+	return 1;
 }
